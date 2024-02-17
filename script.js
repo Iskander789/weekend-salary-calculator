bf8e7e100717662d59145700196aef41c71f0637
Sean Harrison <sharrison9@gmail.com>
2024-02-17 14:11:50 -0600

    added submit button, made handleSubmiit function, first name is logging to console.

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log('js is sourced!');
 
+let totalMonthly = 0;
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    let firstName = document.getElementById("firstName").value;
+
+    console.log('First name entered', firstName);
+
+
+}
+
