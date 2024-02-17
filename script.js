ff34c3ca3dba4eac701da0fe0b8814307356ff18
Sean Harrison <sharrison9@gmail.com>
2024-02-17 14:25:40 -0600

    alll form fields submitting to console

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ let totalMonthly = 0;
 function handleSubmit(event) {
     event.preventDefault();
 
-    let firstName = document.getElementById("firstName").value;
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value
+    let employeeID = document.getElementById('employeeID').value
+    let jobTitle = document.getElementById('jobTitle').value
+    let annualSalary = document.getElementById('annualSalary').value
 
-    console.log('First name entered', firstName);
+    console.log('First name entered: ', firstName);
+    console.log('Last name entered: ', lastName);
+    console.log('Employee ID # entered: ', employeeID);
+    console.log('Job title entered: ', jobTitle);
+    console.log('Annual salary entered: ', annualSalary);
+
+    
 
 
 }
